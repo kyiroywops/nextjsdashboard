63958ccee887d031e45baeeac317d4e2621d41b6
Kylian Leroy <aleexisazul@gmail.com>
2023-08-09 17:08:11 -0400

    mongo

---
+++ Downloads/nextjstesteos/nextjssss/database/db.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+// 0 = disconnected
+// 1 = connected
+// 2 = connecting
+// 3 = disconnecting
+
+const mongooConnection = {
+    isConnected: 0
+}
+
+export const connect = async() => { 
+    if (mongooConnection.isConnected) {
+        console.log("already connected");
+        return;
+    }
+
+    if (mongoose.connections.length > 0) {
+        mongooConnection.isConnected = mongoose.connections[0].readyState;
+        if (mongooConnection.isConnected === 1) {
+            console.log("use previous connection");
+            return;
+        }
+        await mongoose.disconnect();
+
+    }
+
+    await mongoose.connect('....');
+    mongooConnection.isConnected = 1;
+    console.log("new connection");
+}
+
+export const disconnect = async() => {
+    if (mongooConnection.isConnected !== 0) {
+        console.log("no connection to break");
+        return;
+    }
+    await mongoose.disconnect();
+    console.log("disconnected");
+
+
+   
+}
